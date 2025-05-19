0e13b795373ecc7e576e50a290ce58570ef965af
chanakya <chanakyavaleti@gmail.com>
2025-05-19 22:44:41 +0530

    1st task javascript light off and on

---
+++ javascript/task1.js
@@ -0,0 +1,19 @@
+    function onswitch()
+    {
+        document.getElementById("bulbimg").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+        document.getElementById("catimg").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+        
+        document.getElementById("on").style.backgroundColor="white";
+        document.getElementById("off").style.backgroundColor="blue";
+        document.getElementById("butt").textContent = "Switched On";
+    }
+
+    function offswitch()
+    {
+        document.getElementById("bulbimg").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+        document.getElementById("catimg").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+        
+        document.getElementById("off").style.backgroundColor="white";
+        document.getElementById("on").style.backgroundColor="blue";
+        document.getElementById("butt").textContent = "Switched Off";
+    }
